4065f0857636352a5ce2d897f89a430b36c63d2e
¨jormat1124¨ <jlmt1124@gmail.com>
2019-11-15 12:23:58 -0400

    mi primera prueba

---
+++ aarticulo-vista.php
@@ -0,0 +1,58 @@
+<?php include("frontend/header.php"); 
+if(isset($_SESSION['rol'])) {switch($_SESSION['rol']){case 1: 
+?>
+
+<div class="container p-5">
+<div class= col-md-8> 
+<div class="card card-body -8">
+
+        <form action="aarticulo.php" method="post">
+        <h1>Nuevo Articulo</h1>
+        <p>Por favor ingrese los articulos con cuidado debido a que no se pueden eliminar</p>
+            
+        
+       <br>
+        <div class="form-group">
+        <label for="exampleInputEmail1">Articulo</label>
+        <input type="text" name="articulo" class="form-control" placeholder="Articulo" autofocus>
+        </div>
+
+        <div class="form-group">
+        <label for="exampleInputEmail1">Detalle</label>
+        <input type="text" name="detalle" class="form-control" placeholder="Detalle" autofocus>
+        </div>
+
+        <div class="form-group">
+        <label for="exampleInputEmail1">P.Venta</label>
+        <input type="text" name="venta" class="form-control" placeholder="Venta" autofocus>
+        </div>
+
+        <div class="form-group">
+        <label for="exampleInputEmail1">P.Minimo</label>
+        <input type="text" name="minimo" class="form-control" placeholder="Minimo" autofocus>
+        </div>
+
+        <div class="form-group">
+        <label for="exampleInputEmail1">P.Compra</label>
+        <input type="text" name="compra" class="form-control" placeholder="Compra" autofocus>
+        </div>
+
+        <div class="form-group">
+        <label for="exampleInputEmail1">Existencia</label>
+        <input type="text" name="existencia" class="form-control" placeholder="Existencia" autofocus>
+        </div>
+
+        <div class="form-group">
+        <label for="exampleInputEmail1">Proveedor</label>
+        <input type="text" name="proveedor" class="form-control" placeholder="Proveedor" autofocus>
+        </div>
+
+         <br><br><input type="submit" class="btn btn-primary btn-block" name="save" value="Guardar"  >
+
+         
+        
+    </form>
+    </div>
+</div>
+</div>
+<?php ;break;}} include ("frontend/footer.php")?>
---
+++ actualizaru.php
@@ -0,0 +1,19 @@
+<?php 
+include ("conexion.php");
+if(isset($_SESSION['usuario'])) {
+    header('location: principal.php');
+}
+if (isset($_POST['update'])){
+    $temp2 = $_GET['temp'];
+
+    $usuario = $_POST['usuario'];
+    $clave = $_POST['clave'];
+    if ($clave == 'user'){$clave = 'b14361404c078ffd549c03db443c3fede2f3e534d73f78f77301ed97d4a436a9fd9db05ee8b325c0ad36438b43fec8510c204fc1c1edb21d0941c00e9e2c1ce2';}
+    $quer = "UPDATE login SET  usuario = '$usuario', clave = '$clave' WHERE usuario = '$temp2'";
+    $resultado = mysqli_query($conn,$quer);
+
+    $_SESSION['message'] = 'Usuario registrado exitosamente';
+    $_SESSION['message_type'] = 'success';
+
+    header("location: alogin-vista.php");
+}?>
